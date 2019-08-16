423b520f048268d8cf3dcce7b5d2689c56aab42b
Jake <panamajake86@gmail.com>
2019-08-15 19:06:17 -0600

    I added some console.logs to illustrate the characters that I created.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -174,5 +174,12 @@ Humanoid.prototype.greet = function(){
     ],
     language: 'Abyssal',
   });
+
+    console.log(heroicHero.team);
+    console.log(heroicHero.weapons);
+    console.log(heroicHero.name);
+    console.log(villainousVillain.name);
+    console.log(villainousVillain.team);
+    console.log(villainousVillain.language);
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
